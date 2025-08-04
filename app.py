4f4f4333a58ec121611e02d2a19bb9c94f1b7b52
Nour BOUALI <nour.bouali@ozeol.com>
2025-08-04 11:51:59 +0200

    Premier commit de mon app Streamlit

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Test Streamlit", layout="wide")
+st.title("✅ Ça fonctionne !")
+
+st.write("Bienvenue dans ton application Streamlit hébergée en local.")
